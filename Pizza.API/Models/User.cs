61e9aa0bc997c44300e1082864412a660a26454a
tmnpierre <tomyn.pierre@gmail.com>
2024-02-09 13:54:48 +0100

    feat(models): update User

--- Pizza.API/Models/User.cs
+++ Pizza.API/Models/User.cs
@@ -1,32 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pizza.API.Models
 {
     [Table("Utilisateur")]
     public class User
     {
-        [Column("Id")]
+        [Column("Id")] [Display(Name = "Id")]
         public int Id { get; set; }
 
-        [Column("Prénom")]
+        [Column("Prénom")] [Display(Name = "Prénom")]
         public string? FirstName { get; set; }
 
-        [Column("Nom de famille")]
+        [Column("Nom de famille")] [Display(Name = "Nom de famille")]
         public string? LastName { get; set; }
 
-        [Column("Date de naissance")]
+        [Column("Date de naissance")] [Display(Name = "Date de naissance")]
         public DateTime DateOfBirth { get; set; }
 
-        [Column("Email")]
+        [Column("Email")] [Display(Name = "Email")]
         public string? Email { get; set; }
 
-        [Column("Numéro de téléphone")]
+        [Column("Numéro de téléphone")] [Display(Name = "Numéro de téléphone")]
         public string? PhoneNumber { get; set; }
 
-        [Column("id")]
+        [Column("Adresse postale")] [Display(Name = "Adresse postale")]
         public string? Address { get; set; }
 
-        [Column("id")]
+        [Column("Mot de passe")] [Display(Name = "Mot de passe")]
         public string? Password { get; set; } 
     }
 }
