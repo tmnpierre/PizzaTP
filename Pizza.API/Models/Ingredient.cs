4dd703096e45a825d040a212de631d9c2673fa4e
tmnpierre <tomyn.pierre@gmail.com>
2024-02-09 15:11:18 +0100

    feat(models): update Ingredient

--- Pizza.API/Models/Ingredient.cs
+++ Pizza.API/Models/Ingredient.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pizza.API.Models
 {
@@ -9,12 +10,15 @@ namespace Pizza.API.Models
         public int Id { get; set; }
 
         [Column("Nom")]
+        [Display(Name = "Nom de l'ingrédient")]
         public string? Name { get; set; }
 
         [Column("Description")]
+        [Display(Name = "Description de l'ingredient")]
         public string? Description { get; set; }
 
         [Column("Prix")]
+        [Display(Name = "Prix de l'ingredient")]
         public decimal Price { get; set; }
     }
 }
