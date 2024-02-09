c5be8b1393fac68f3e2336d3dc3d5a6adabb4b8b
tmnpierre <tomyn.pierre@gmail.com>
2024-02-09 14:38:51 +0100

    feat(models): update Pizza

--- Pizza.API/Models/Pizza.cs
+++ Pizza.API/Models/Pizza.cs
@@ -1,12 +1,26 @@
-﻿namespace Pizza.API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pizza.API.Models
 {
+    [Table("Pizza")]
     public class Pizza
     {
+        [Column("Id")]
         public int Id { get; set; }
+
+        [Column("Nom")]
         public string? Name { get; set; }
+
+        [Column("Description")]
         public string? Description { get; set; }
+
+        [Column("Prix")]
         public decimal Price { get; set; }
+
+        [Column("Aperçu")]
         public string? Overview { get; set; }
+
+        [Column("Categorie")]
         public Categorie Categorie { get; set; }
     }
 
