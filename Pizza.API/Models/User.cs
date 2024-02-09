3860ee797b1cf582a6138ce4d90148ce5ff6ec8b
tmnpierre <tomyn.pierre@gmail.com>
2024-02-09 13:43:32 +0100

    feat(models): update User

--- Pizza.API/Models/User.cs
+++ Pizza.API/Models/User.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? MailingAddress { get; set; }
