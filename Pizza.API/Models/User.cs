d6647bf1b56dd2017cef8ab45ede851db1b6839a
tmnpierre <tomyn.pierre@gmail.com>
2024-02-09 14:00:24 +0100

    feat(models): update User

--- Pizza.API/Models/User.cs
+++ Pizza.API/Models/User.cs
@@ -6,28 +6,28 @@ namespace Pizza.API.Models
     [Table("Utilisateur")]
     public class User
     {
-        [Column("Id")] [Display(Name = "Id")]
+        [Column("Id")] [Display(Name = "Id")] 
         public int Id { get; set; }
 
-        [Column("Prénom")] [Display(Name = "Prénom")]
+        [Column("Prénom")] [Display(Name = "Prénom")] [DataType(DataType.Text)]
         public string? FirstName { get; set; }
 
-        [Column("Nom de famille")] [Display(Name = "Nom de famille")]
+        [Column("Nom de famille")] [Display(Name = "Nom de famille")] [DataType(DataType.Text)]
         public string? LastName { get; set; }
 
-        [Column("Date de naissance")] [Display(Name = "Date de naissance")]
+        [Column("Date de naissance")] [Display(Name = "Date de naissance")] [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
-        [Column("Email")] [Display(Name = "Email")]
+        [Column("Email")] [Display(Name = "Email")] [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
 
-        [Column("Numéro de téléphone")] [Display(Name = "Numéro de téléphone")]
+        [Column("Numéro de téléphone")] [Display(Name = "Numéro de téléphone")] [DataType(DataType.PhoneNumber)]
         public string? PhoneNumber { get; set; }
 
-        [Column("Adresse postale")] [Display(Name = "Adresse postale")]
+        [Column("Adresse postale")] [Display(Name = "Adresse postale")] [DataType(DataType.MultilineText)]
         public string? Address { get; set; }
 
-        [Column("Mot de passe")] [Display(Name = "Mot de passe")]
+        [Column("Mot de passe")] [Display(Name = "Mot de passe")] [DataType(DataType.Password)]
         public string? Password { get; set; } 
     }
 }
