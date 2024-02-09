d2c706eba777539a407ec9b46722ababda617365
tmnpierre <tomyn.pierre@gmail.com>
2024-02-09 12:29:30 +0100

    initial commit

--- Pizza.API/Models/User.cs
+++ Pizza.API/Models/User.cs
@@ -2,5 +2,12 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? MailingAddress { get; set; }
+        public string? Password { get; set; } 
     }
 }
