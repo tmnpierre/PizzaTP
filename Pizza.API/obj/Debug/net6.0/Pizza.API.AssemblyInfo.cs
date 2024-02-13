6de550ccfc8e65124567e86864c253fe808ee53e
tmnpierre <tomyn.pierre@gmail.com>
2024-02-13 09:44:26 +0100

    initial commit

--- Pizza.API/obj/Debug/net6.0/Pizza.API.AssemblyInfo.cs
+++ Pizza.API/obj/Debug/net6.0/Pizza.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pizza.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b83cb8435eb112245c69fd0762a6a50928a6e39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0bca2f76cf62beb445252187476ed5464134633")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pizza.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pizza.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
